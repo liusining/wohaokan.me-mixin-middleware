5682f39aedff5648b963fbea85a12669e8cb2e3a
liusining <siningdream@gmail.com>
2018-12-30 22:20:44 +0800

    Feature: get user info

--- go.mod
+++ go.mod
@@ -1,1 +1,16 @@
 module github.com/liusining/wohaokan.me-mixin-middleware
+
+require (
+	github.com/MixinNetwork/bot-api-go-client v0.0.0-20181111105712-d6ba17782cb2
+	github.com/MixinNetwork/go-number v0.0.0-20180814121220-f48e2574d9ef // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
+	github.com/spf13/viper v1.3.1
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	mixin "github.com/MixinNetwork/bot-api-go-client"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("loading config file: %s", err))
+	}
+	fmt.Println("Config loaded")
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(func(c *gin.Context) {
+		reqID, _ := c.GetPostForm("request_id")
+		c.Set("RequestID", reqID)
+		fmt.Printf("RequestID: %s\n", reqID)
+		c.Next()
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "pong",
+		})
+	})
+	r.POST("/auth_info", authInfo)
+	r.Run(viper.GetString("service.bind_ip"))
+}
+
+func authInfo(c *gin.Context) {
+	ctx := context.Background()
+	fmt.Println("Applying for an access token")
+	authCode, _ := c.GetPostForm("auth_code")
+	accessToken, scope, err := mixin.OAuthGetAccessToken(ctx,
+		viper.GetString("mixin.client_id"),
+		viper.GetString("mixin.client_secret"),
+		authCode, "")
+	fmt.Printf("\nDone\n")
+	if err != nil {
+		apiError(c, err)
+		return
+	}
+	user, err := mixin.UserMe(ctx, accessToken)
+	if err != nil {
+		apiError(c, err)
+		return
+	}
+	c.JSON(200, gin.H{
+		"access_token":    accessToken,
+		"scope":           scope,
+		"user_id":         user.UserId,
+		"session_id":      user.SessionId,
+		"pin_token":       user.PinToken,
+		"identity_number": user.IdentityNumber,
+		"full_name":       user.FullName,
+		"avatar_url":      user.AvatarURL,
+	})
+}
+
+func apiError(c *gin.Context, err error) {
+	c.JSON(400, gin.H{
+		"err": fmt.Sprintf("%s", err),
+	})
+	fmt.Printf("err: %s\n", err)
+}
